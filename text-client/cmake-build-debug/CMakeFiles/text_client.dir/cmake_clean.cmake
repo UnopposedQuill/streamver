e61aba5fd03fea063d91b678fbc601f546537039
Esteban <darklightbeing@gmail.com>
2020-07-02 17:01:38 -0600

    Proyectos con CLion
    Se crean proyectos base con CLion para trabajar el proyecto.

---
+++ text-client/cmake-build-debug/CMakeFiles/text_client.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/text_client.dir/main.c.o"
+  "text_client"
+  "text_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/text_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
